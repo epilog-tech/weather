47a6ec738bd63ff9716153d1c862c9268c5c8d21
Aymen BEN MILED <benmiledaymen@hotmail.com>
2024-08-13 18:30:21 +0200

    add deux component weather and weather-list and rooter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    NgbModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
+import { WeatherListComponent } from './components/weather-list/weather-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',redirectTo:'weather', pathMatch:'full' },
+    { path: 'weather', component:WeatherComponent, title: 'weather' },
+    { path: 'weather-list', component:WeatherListComponent, title: 'weather-list' }
+];
