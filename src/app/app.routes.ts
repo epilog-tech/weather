6cf48deebc37e3e1298f64e3184abaca20a75c5b
Aymen BEN MILED <benmiledaymen@hotmail.com>
2024-08-13 20:12:05 +0200

    try fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { WeatherComponent } from './components/weather/weather.component';
 import { WeatherListComponent } from './components/weather-list/weather-list.component';
 
 export const routes: Routes = [
-    { path: '',redirectTo:'weather', pathMatch:'full' },
-    { path: 'weather', component:WeatherComponent, title: 'weather' },
-    { path: 'weather-list', component:WeatherListComponent, title: 'weather-list' }
+    { path: '/weather/',redirectTo:'/weather/weather', pathMatch:'full' },
+    { path: '/weather/weather', component:WeatherComponent, title: 'weather' },
+    { path: '/weather/weather-list', component:WeatherListComponent, title: 'weather-list' }
 ];
